b6bb45bcb417db6ddd31fa280bf9271046dec9f2
Muhammad mahdi <danyalm50@gmail.com>
2020-10-18 21:45:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,4 +9,12 @@ end
 
 display_board = [" "," "," "," "," "," "," "," "," "]
 
+def display_board(rows[2])
+puts ("   |   |   ")
+puts ("-----------")
+puts ("   | X |   ")
+puts ("-----------")
+puts ("   |   |   ")
+end
+
 display_board = [" "," "," "," ","X"," "," "," "," "]
