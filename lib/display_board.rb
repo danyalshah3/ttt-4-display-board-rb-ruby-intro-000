06538ed1fc70c2d4abd20c58741f15e4fcad9694
Muhammad mahdi <danyalm50@gmail.com>
2020-10-18 22:13:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board(rows)
-  puts ("   |   |   ")
-  puts ("-----------")
-  puts ("   |   |   ")
-  puts ("-----------")
-  puts ("   |   |   ")
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 
