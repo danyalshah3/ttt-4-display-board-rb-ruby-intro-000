f99bc5675a8aa8ba8689701eb98cca58d5e04d34
Muhammad mahdi <danyalm50@gmail.com>
2020-10-18 21:35:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,17 +1,10 @@
 def display_board(rows)
-  puts ("   |   |   ")
-  puts ("-----------")
-  puts ("   |   |   ")
-  puts ("-----------")
-  puts ("   |   |   ")
+puts ("   |   |   ")
+puts ("-----------")
+puts ("   |   |   ")
+puts ("-----------")
+puts ("   |   |   ")
 end
 
 
 display_board([" "," "," "," "," "," "," "," "," "])
-
-def display_board(rows)
-puts " The #{board[4] = O}"
-end
-
-
-board = [" "," "," "," ","X"," "," "," "," "]
