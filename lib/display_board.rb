498bb89f9753f305f56f545da278998806c7ac55
Muhammad mahdi <danyalm50@gmail.com>
2020-10-18 17:10:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -86,11 +86,11 @@ end
 display_board([" "," ","O"," ","O"," ","O"," "," "])
 
 def display_board(rows)
-  puts " X | O | X "
+  puts " O | X |   "
   puts "-----------"
-  puts " X | O | O "
+  puts "   | X |   "
   puts "-----------"
-  puts " X | O | O "
+  puts " X | O |   "
 end
 
-display_board(["X","O","X","X","O","O","X","O","O"])
+display_board(["O","X"," "," ","X"," ","X","O"," "])
